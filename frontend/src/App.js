c8adee66c7494a9ac88523205caaa9ca87cea243
ItzelSelena <selena65524@live.com>
2024-01-20 15:14:27 -0600

    django unificado con react funcional

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+//este logo es de react xD se puede quitar cuando se requiera :3 
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.!! prueba :3
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React aiuda
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

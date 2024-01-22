65469866cb0c957bb2a0968a02afcabd41a8061b
ItzelSelena <selena65524@live.com>
2024-01-21 19:43:48 -0600

    Co-authored-by: matamancos3000 <matamancos3000@users.noreply.github.com>

--- frontend/src/components/MainDash/MainDash.jsx
+++ frontend/src/components/MainDash/MainDash.jsx
@@ -5,7 +5,9 @@ import "./MainDash.css";
 const MainDash = () => {
   return (
     <div className="MainDash">
-      <h1>Indicadores!!!!!!!!!x2</h1>
+      <h1> </h1>
+      
+      
       <Cards/>
       <Table/>
     </div>
